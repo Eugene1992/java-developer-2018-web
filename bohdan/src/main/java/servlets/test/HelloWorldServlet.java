b5bff169e2ae77a1403d794473f90bac7ad46e42
DanTe067 <teslyabya@gmail.com>
2018-08-14 14:39:42 +0300

    crud resp added

--- bohdan/src/main/java/servlets/test/HelloWorldServlet.java
+++ bohdan/src/main/java/servlets/test/HelloWorldServlet.java
@@ -3,29 +3,34 @@ package servlets.test;
 import JDBC_pckg.Employee;
 import JDBC_pckg.dao_pckg.EmployeeDaoJDBCImpl;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Comparator;
 import java.util.List;
 
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        try {
-            Class.forName("JDBC_pckg.Employee");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+
+        PrintWriter writer = resp.getWriter();
+
         EmployeeDaoJDBCImpl crud = new EmployeeDaoJDBCImpl();
         List<Employee> employees = crud.getAll();
-        PrintWriter writer = resp.getWriter();
-//        writer.write("Hello world!");
-        employees.
-                forEach(employee -> writer.write(employee.toString() + "\n"));
+
+        employees.sort(new Comparator<Employee>() {
+            @Override
+            public int compare(Employee firstEmp, Employee secondEmp) {
+                return firstEmp.getId() - secondEmp.getId();
+            }
+        });
+
+        employees
+                .forEach(employee -> writer.write(employee.toString() + "\n"));
+
     }
 
 }
